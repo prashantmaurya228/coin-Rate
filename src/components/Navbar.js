2656483bae814c273cb0520fd2d832be6c461573
prashantmaurya228 <34316534+prashantmaurya228@users.noreply.github.com>
2023-05-09 00:46:05 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <Link to='/'>
+            <div className='navbar'>
+                <h1> Coin <span className='rate'>Rate</span></h1>
+            </div>
+        </Link>
+    )
+}
+
+export default Navbar
